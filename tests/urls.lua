492680d32c75f4cba14a0fe90f5839422b005b8c
horse-dog <1418411576@qq.com>
2026-09-13 01:18:50 +0800

    render

--- tests/urls.lua
+++ tests/urls.lua
@@ -1,6 +1,6 @@
 local paths = {"/index", "/login", "/picture", "/register", "/video", "/index.html",
                "/picture.html", "/video.html", "/welcome.html",
-               "/hello", "/status", "/ping", "/template"}
+               "/hello", "/status", "/ping"}
 
 request = function()
     return wrk.format(nil, paths[math.random(#paths)])
